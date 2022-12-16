3f517885709379a9c8b5ea44fdd973feae5f6aed
aoxanDiscovery <alvaro_oxandabarat@discovery.com>
2022-12-16 16:05:39 -0300

    minor change

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,9 @@
-package src.main.java;
-
 public class Test {
 
     public static void main(String[] args){
+
         System.out.println("Hello There!");
+        System.out.println("Bye!");
+        System.out.println("Done!");
     }
 }
